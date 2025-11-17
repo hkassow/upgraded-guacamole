34696c6d43b2b18a6041274566c5dd0e341e54c0
hkassow <hunterkassow@protonmail.com>
2025-11-17 10:08:16 +0100

    update server to run on high port + check test api key

---
+++ gouda-woulda/go.mod
@@ -0,0 +1,3 @@
+module gouda-woulda
+
+go 1.25.4
--- gouda-woulda/server.go
+++ gouda-woulda/server.go
@@ -1,7 +1,32 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"io"
+	"log"
+	"net/http"
+)
+
+type ParseRequest struct {
+	Prompt string `json:"prompt"`
+}
+
+type OllamaRequest struct {
+	Model string `json:"model"`
+	Prompt string `json:"prompt"`
+	Stream bool `json:"stream"`
+}
+
+type OllamaResponse struct {
+	Response string `json:"response"`
+}
+
 func parseHandler(w http.ResponseWriter, r *http.Request) {
+    log.Println("Hello from the ai server")
     // ---- API KEY CHECK ----
     apiKey := r.Header.Get("X-API-Key")
-    if apiKey != InternalAPIKey {
+    if apiKey != "test-api-key" {
         http.Error(w, "Unauthorized", http.StatusUnauthorized)
         return
     }
@@ -12,21 +37,24 @@ func parseHandler(w http.ResponseWriter, r *http.Request) {
         return
     }
 
-    var req PredictRequest
+    var req ParseRequest
     if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
         http.Error(w, err.Error(), http.StatusBadRequest)
         return
     }
 
+    log.Printf("Prompt: %s\n", string(req.Prompt))
+
     ollamaReq := OllamaRequest{
-        Model:  "llama3.2",
-        Prompt: req.Input,
+	Model:  "llama3:70b-instruct-q3_K_M",
+        Prompt: req.Prompt,
+	Stream: false,
     }
 
     jsonReq, _ := json.Marshal(ollamaReq)
 
     resp, err := http.Post(
-        "http://localhost:11434/api/generate",
+        "http://localhost:11434/api/generate?stream=false",
         "application/json",
         bytes.NewBuffer(jsonReq),
     )
@@ -38,16 +66,29 @@ func parseHandler(w http.ResponseWriter, r *http.Request) {
 
     body, _ := io.ReadAll(resp.Body)
 
+
+    log.Printf("Final body %s\n", string(body))
+    var ollamaResp OllamaResponse
+
+    if err := json.Unmarshal(body, &ollamaResp); err != nil {
+    	http.Error(w, "Failed to parse model response", 500)
+	return
+    }
+    
+    log.Printf("Output: %s\n", ollamaResp)
+
     w.Header().Set("Content-Type", "application/json")
-    w.Write(body)
+    json.NewEncoder(w).Encode(map[string]string{
+	    "result": ollamaResp.Response,
+    })
 }
 
 func main() {
 	http.HandleFunc("/parse-recipe", parseHandler)
 
-	log.Println("Starting HTTPS server on port 443...")
+	log.Println("Starting HTTPS server on port 8556...")
 	err := http.ListenAndServeTLS(
-		":443",
+		":8556",
 		"server.crt",
 		"server.key",
 		nil,
